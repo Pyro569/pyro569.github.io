d5c26546b285225b375e6d330ae67fc17eb1d247
Pyro <79991107+Pyro569@users.noreply.github.com>
2023-05-24 21:12:46 -0400

    Windows 98 Desktop

--- javascript/index.js
+++ javascript/index.js
@@ -10,3 +10,13 @@ function home() {
   window.location = "index.html";
   console.log("Returning to homepage");
 }
+
+function redirect(page) {
+  window.location.href = page;
+}
+
+function openTextFile(page) {
+  document.getElementById('textFrame').style.display = "block";
+  document.getElementById('textFrame').setAttribute('data', page);
+  console.log('Opened text file ' + page + ' on desktop');
+}
