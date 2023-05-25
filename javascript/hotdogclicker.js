c59cd0be14ef792cf7208678a8f1c43d1278ff62
Pyro <79991107+Pyro569@users.noreply.github.com>
2023-05-25 03:37:55 -0400

    Better Hotdog Clicker

--- javascript/hotdogclicker.js
+++ javascript/hotdogclicker.js
@@ -7,11 +7,11 @@ var vendors = 0;
 
 
 function updateStats() {
-    document.getElementById("hotdogCounter").textContent = "Hotdogs: " + hotdogs;
+    document.getElementById("hotdogCounter").textContent = "Hotdogs: " + display(hotdogs);
 }
 
 function updateCosts() {
-    document.getElementById("buyVendor").textContent = "Buy Hotdog Vendor (" + ((vendors * 12)+12) + " hotdogs)";
+    document.getElementById("buyVendor").textContent = "Buy Hotdog Vendor (" + display(((vendors * 12)+12)) + " hotdogs)";
 }
 
 function update() {
@@ -25,7 +25,7 @@ function addHPS() {
 
 function clickHotdog() {
     hotdogs += hpc;
-    document.getElementById("hotdogCounter").textContent = "Hotdogs: " + hotdogs;
+    document.getElementById("hotdogCounter").textContent = "Hotdogs: " + display(hotdogs);
     updateStats();
 }
 
@@ -35,9 +35,65 @@ function startClicker() {
 
 function buyVendor() {
     if (hotdogs >= (vendors*12)+12) {
-        hotdogs -= 12;
+        hotdogs -= (vendors*12)+12;
         hps += vendorHPS;
         vendors += 1;
         update();
+    }
+}
+
+function display(number) {
+		if(number < 1000000) {
+    		return number;
+    } else if(number   < 1000000000) {
+    		return Math.round(number / 10000) / 100 + " M";
+    } else if(number   < 1000000000000) {
+    		return Math.round(number / 10000000) / 100 + " B";
+    } else if(number   < 1000000000000000) {
+    		return Math.round(number / 10000000000) / 100 + " T";
+    } else if(number   < 1000000000000000000) {
+    		return Math.round(number / 10000000000000) / 100 + " Quad";
+    } else if(number   < 1000000000000000000000) {
+    		return Math.round(number / 10000000000000000) / 100 + " Quin";
+    } else if(number   < 1000000000000000000000000) {
+    		return Math.round(number / 10000000000000000000) / 100 + " Sex";
+    } else if(number   < 1000000000000000000000000000) {
+    		return Math.round(number / 10000000000000000000000) / 100 + " Sep";
+    } else if(number   < 1000000000000000000000000000000) {
+    		return Math.round(number / 10000000000000000000000000) / 100 + " Oct";
+    } else if(number   < 1000000000000000000000000000000000) {
+    		return Math.round(number / 10000000000000000000000000000) / 100 + " Non";
+    } else if(number   < 1000000000000000000000000000000000000) {
+    		return Math.round(number / 10000000000000000000000000000000) / 100 + " Dec";
+    } else if(number   < 1000000000000000000000000000000000000000) {
+    	  return Math.round(number / 10000000000000000000000000000000000) / 100 + " Undec";
+    } else if(number   < 1000000000000000000000000000000000000000000) {
+    	  return Math.round(number / 10000000000000000000000000000000000000) / 100 + " Duodec";
+    } else if(number   < 1000000000000000000000000000000000000000000000) {
+    	  return Math.round(number / 10000000000000000000000000000000000000000) / 100 + " Tredec";
+    } else if(number   < 1000000000000000000000000000000000000000000000000) {
+    	  return Math.round(number / 10000000000000000000000000000000000000000000) / 100 + " Quatdec";
+    } else if(number   < 1000000000000000000000000000000000000000000000000000) {
+    	  return Math.round(number / 10000000000000000000000000000000000000000000000) / 100 + " Quindec";
+    } else if(number   < 1000000000000000000000000000000000000000000000000000000) {
+    	  return Math.round(number / 10000000000000000000000000000000000000000000000000) / 100 + " Sexdec";
+    } else if(number   < 1000000000000000000000000000000000000000000000000000000000) {
+    	  return Math.round(number / 10000000000000000000000000000000000000000000000000000) / 100 + " Septen";
+    } else if(number   < 1000000000000000000000000000000000000000000000000000000000000) {
+    	  return Math.round(number / 10000000000000000000000000000000000000000000000000000000) / 100 + " Octodec";
+    } else if(number   < 1000000000000000000000000000000000000000000000000000000000000000) {
+    	  return Math.round(number / 10000000000000000000000000000000000000000000000000000000000) / 100 + " Novendec";
+    } else if(number   < 1000000000000000000000000000000000000000000000000000000000000000000) {
+    	  return Math.round(number / 10000000000000000000000000000000000000000000000000000000000000) / 100 + " Vigin";
+    } else if(number   < 1000000000000000000000000000000000000000000000000000000000000000000000) {
+    	  return Math.round(number / 10000000000000000000000000000000000000000000000000000000000000000) / 100 + " Unvigin";
+    } else if(number   < 1000000000000000000000000000000000000000000000000000000000000000000000000) {
+    	  return Math.round(number / 10000000000000000000000000000000000000000000000000000000000000000000) / 100 + " That's A Lot of Hotdogs";
+    }
+}
+  
+const dev = {
+    add(amount) {
+        hotdogs += amount;
     }
 }
