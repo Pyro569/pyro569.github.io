9fd4459ea305eb5d52984493b99d366a36c9f9de
Pyro <79991107+Pyro569@users.noreply.github.com>
2023-05-25 23:42:11 -0400

    Kind of working hotdog clicker cookie saving

--- javascript/hotdogclicker.js
+++ javascript/hotdogclicker.js
@@ -1,13 +1,20 @@
 var hotdogs = 0;
 var hps = 0;
 var hpc = 1;
+
 var vendorHPS = 2;
 var storeHPS = 15;
 var factoryHPS = 50;
+var carnivalHPS = 75;
+var powerplantHPS = 150;
+var countryHPS = 250;
 
 var vendors = 0;
 var stores = 0;
 var factories = 0;
+var carnivals = 0;
+var powerplants = 0;
+var countries = 0;
 
 
 function updateStats() {
@@ -21,6 +28,9 @@ function updateCosts() {
     document.getElementById("buyVendor").textContent = "Buy Hotdog Vendor (" + display(((vendors * 12) + 12)) + " hotdogs)";
     document.getElementById("buyStore").textContent = "Buy Hotdog Store (" + display(((stores * 150) + 150)) + " hotdogs)";
     document.getElementById("buyFactory").textContent = "Buy Hotdog Factory (" + display(((factories * 500) + 500)) + " hotdogs)";
+    document.getElementById("buyCarnival").textContent = "Buy Hotdog Carnival (" + display(((carnivals * 1500) + 1500)) + " hotdogs)";
+    document.getElementById("buyPowerPlant").textContent = "Buy Hotdog Power Plant (" + display(((powerplants * 5500) + 5500)) + " hotdogs)";
+    document.getElementById("buyCountry").textContent = "Buy Hotdog Country (" + display(((countries * 25000) + 25000)) + " hotdogs)";
 }
 
 function update() {
@@ -65,6 +75,33 @@ function buyFactory() {
         hotdogs -= ((factories * 500) + 500);
         hps += factoryHPS;
         factories += 1;
+        update();
+    }
+}
+
+function buyCarnival() {
+    if (hotdogs >= (carnivals * 1500) + 1500) {
+        hotdogs -= ((carnivals * 1500) + 1500);
+        hps += carnivalHPS;
+        carnivals += 1;
+        update();
+    }
+}
+
+function buyPowerPlant() {
+    if (hotdogs >= (powerplants * 5500) + 5500) {
+        hotdogs -= ((powerplants * 5500) + 5500);
+        hps += powerplantHPS;
+        powerplants += 1;
+        update();
+    }
+}
+
+function buyCountry() {
+    if (hotdogs >= (countries * 25000) + 25000) {
+        hotdogs -= ((countries * 25000) + 25000);
+        hps += countryHPS;
+        countries += 1;
         update();
     }
 }
@@ -117,4 +154,51 @@ function display(number) {
     } else if(number   < 1000000000000000000000000000000000000000000000000000000000000000000000000) {
     	  return Math.round(number / 10000000000000000000000000000000000000000000000000000000000000000000) / 100 + " That's A Lot of Hotdogs";
     }
+}
+
+function save() {
+    makeCookie("hotdogs", hotdogs);
+    makeCookie("hps", hps);
+    makeCookie("hpc", hpc);
+    makeCookie("vendors", vendors);
+    makeCookie("stores", stores);
+    makeCookie("carnivals", carnivals);
+    makeCookie("powerplants", powerplants);
+    makeCookie("countries", countries);
+}
+
+function load() {
+    hotdogs = getCookie("hotdogs");
+    hps = getCookie("hps")
+    hpc = getCookie("hpc");
+    vendors = getCookie("vendors");
+    stores = getCookie("stores");
+    carnivals = getCookie("carnivals");
+    powerplants = getCookie("powerplants");
+    countries = getCookie("countries");
+    update();
+}
+
+function getCookie(cname) {
+    var name = cname + '=';
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return '';
+}
+
+function getCookieValue(name) {
+    return parseInt(getCookie(name));
+}
+
+function makeCookie(name, value) {
+    document.cookie = name + "=" + value;
 }
